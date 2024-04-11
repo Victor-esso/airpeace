e9616a3300cdebdb2b54af65c7e968379c7b125d
Victor Esso Nmakwe <victoresso97@gmail.com>
2024-04-12 00:23:59 +0100

    Remove Logo animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {GlobalContext} from "../providers/Global"
 import { useContext, useState } from "react";
 import * as MaterialDesign from "react-icons/md";
 import {motion} from "framer-motion"
-import animations from '../animateions';
+
 const Header = () => {
     const {
         windowWidth
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <header className="h-[88px] w-full fixed top-0 bg-white z-50">
         <div className="container mx-auto h-full horizontal center-h justify-between px-3 relative">
-            <motion.img animate={{x:0,opacity:1,transition:{ease:"easeInOut", duration:.8,delay:.2 , bounce:.5, type: "spring"}}} initial={{x:-50,opacity:0}}  src={Logo} alt="Airpeace Logo" />
+            <img  src={Logo} alt="Airpeace Logo" />
             <button className="lg:hidden w-[50px] h-[50px] grid place-items-center rounded-md hover:bg-slate-100" onClick={()=>setNavbarVisibility(!navBarVisible)}> {navBarVisible ? <MaterialDesign.MdOutlineClose size={24} />  : <img src={MenuIcon} alt="" />}</button>
             {(windowWidth >= 1024 || (windowWidth <1024 && navBarVisible) ) && <Nav closeNavbar={() => setNavbarVisibility(false)} />}
             
